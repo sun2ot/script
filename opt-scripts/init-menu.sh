aac477d81355f0b4aaa524ff617080578e7e9b8e
sun2ot <sun2ot@qq.com>
2024-04-19 15:46:47 +0800

    soft link replace copy file

--- opt-scripts/init-menu.sh
+++ opt-scripts/init-menu.sh
@@ -2,6 +2,18 @@
 
 # 准备执行menu.sh所需的前置脚本
 
-cp .bash_profile .zsh_profile /etc/skel
-cp chsh-zsh.sh miniconda3.sh proxy.sh /usr/local/script
-cp menu.sh /usr/local/bin/
+ln -s .bash_profile /etc/skel/.bash_profile
+ln -s .zsh_profile /etc/skel/.zsh_profile
+
+if [ -d "/usr/local/script" ]; then
+    echo "/usr/local/script already exists."
+else
+    mkdir -p /usr/local/script
+fi
+
+ln -s chsh-zsh.sh /usr/local/script/chsh-zsh.sh
+ln -s miniconda3.sh /usr/local/script/miniconda3.sh
+ln -s proxy.sh /usr/local/script/proxy.sh
+
+ln -s menu.sh /usr/local/bin/menu
+
