3415ab4a6371bf2233af288dd0c8bde859175414
sun2ot <sun2ot@qq.com>
2024-04-19 18:37:43 +0800

    fix bug

--- opt-scripts/init-menu.sh
+++ opt-scripts/init-menu.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # 准备执行menu.sh所需的前置脚本
+# 注意，需在opt-scripts路径下执行该脚本
 
-ln -s .bash_profile /etc/skel/.bash_profile
-ln -s .zsh_profile /etc/skel/.zsh_profile
+ln -s $(realpath .bash_profile) /etc/skel/.bash_profile
+ln -s $(realpath .zsh_profile) /etc/skel/.zsh_profile
 
 if [ -d "/usr/local/script" ]; then
     echo "/usr/local/script already exists."
@@ -11,9 +12,9 @@ else
     mkdir -p /usr/local/script
 fi
 
-ln -s chsh-zsh.sh /usr/local/script/chsh-zsh.sh
-ln -s miniconda3.sh /usr/local/script/miniconda3.sh
-ln -s proxy.sh /usr/local/script/proxy.sh
+ln -s $(realpath chsh-zsh.sh) /usr/local/script/chsh-zsh.sh
+ln -s $(realpath miniconda3.sh) /usr/local/script/miniconda3.sh
+ln -s $(realpath proxy.sh) /usr/local/script/proxy.sh
 
-ln -s menu.sh /usr/local/bin/menu
+ln -s $(realpath menu.sh) /usr/local/bin/menu
 
