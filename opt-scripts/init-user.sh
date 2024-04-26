f25344c70402a3064a7866cef4c9e6378638b132
sun2ot <84566015+sun2ot@users.noreply.github.com>
2024-04-26 13:33:28 +0800

    change home path to NFS

--- opt-scripts/init-user.sh
+++ opt-scripts/init-user.sh
@@ -11,22 +11,33 @@ fi
 
 user="$1"
 
-# 检查目录是否存在，如果不存在则创建目录并修改所有权和权限
+# 检查NFS下是否存在用户存储，如果不存在则创建目录并修改所有权和权限
 if [ ! -d "/private/$user" ]; then
     mkdir "/private/$user"
     chown "$user:$user" "/private/$user"
     chmod 755 "/private/$user"
 fi
 
-# 确保/home/$user目录存在，否则ln命令会失败
-if [ ! -d "/home/$user" ]; then
-    echo "Error: Home directory /home/$user does not exist."
-    exit 1
-fi
+## Deprecated, please use v2.0
 
-# 创建指向 NAS 的符号链接
-if [ ! -L "/home/$user/$user" ]; then
+# v1.0 
+# 确保/home/$user目录存在，否则ln命令会失败
+# if [ ! -d "/home/$user" ]; then
+#     echo "Error: Home directory /home/$user does not exist."
+#     exit 1
+# fi
+
+# 创建指向NFS的符号链接
+# if [ ! -L "/home/$user/$user" ]; then
+#     ln -s "/private/$user" "/home/$user"
+# fi
+
+# v2.0 
+# 直接将NFS挂载到/home/$user
+if [ ! -d "/home/$user" ]; then
     ln -s "/private/$user" "/home/$user"
+else
+    echo "Warning: /home/$user already exists, skipping."
 fi
 
 cat << EOF > "/home/$user/README.txt"
@@ -45,8 +56,8 @@ cat << EOF > "/home/$user/README.txt"
 
 ## Warning!!!
 
-1. "~/$user/" 这个路径为 "/private/$user" 的软链接，指向NAS，可以持久化存储数据。
-2. "~/" 即你的家目录是一个虚拟环境，一旦环境重启即会丢失(除非备份了镜像)，所以如无必要，**禁止在这里存放任何关键数据**。
+1. "~/" 这个路径为 "/private/$user" 的软链接，指向NFS，可以持久化存储数据。
+2. AIOS 平台是一个容器化的虚拟环境，一旦环境重启即会丢失NFS外的所有数据(除非备份了镜像)，所以如无必要，**禁止在这里存放任何关键数据**。
 3. 如果需要上传数据，可以通过 H3C 网页端的 FTP，也可以 SFTP，不过端口记得改为你SSH连接的端口。如果是从A407机房的服务器往这里拷贝数据，通过SFTP/rsync均可。
 EOF
 
