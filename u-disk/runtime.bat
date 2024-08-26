4877d7d0d3ab8412e8f1acd52b7d3e621f1c026e
sun2ot <84566015+sun2ot@users.noreply.github.com>
2024-08-26 17:05:26 +0800

    update 动态版本获取

--- u-disk/runtime.bat
+++ u-disk/runtime.bat
@@ -13,17 +13,24 @@ echo 10. 退出
 echo -----------------------------------------------------------
 echo.
 
-
 :: 获取用户输入
 set /p choice=请输入您的选择（1-10）：
+
+:: 动态查找最新版本的软件路径
+set "search_path=Ops\Runtime"
+set "git_installer="
+for /f "delims=" %%f in ('dir /b /od "%search_path%\Git-*-64-bit.exe"') do set "git_installer=%%f"
+
+set "anaconda_installer="
+for /f "delims=" %%f in ('dir /b /od "%search_path%\Anaconda3-*-Windows-x86_64.exe"') do set "anaconda_installer=%%f"
 
 :: 根据用户输入执行对应操作
 if "!choice!"=="1" (
     call    Ops\Runtime\VC-Runtime-All-in-One\install_all.bat
 ) else if "!choice!"=="2" (
-    start   Ops\Runtime\Anaconda3-2024.02-1-Windows-x86_64.exe
+    start   "%search_path%\!anaconda_installer!"
 ) else if "!choice!"=="3" (
-    start   Ops\Runtime\Git-2.45.2-64-bit.exe
+    start   "%search_path%\!git_installer!"
 ) else if "!choice!"=="4" (
     start   Ops\Runtime\DotNet-desktop-runtime-8.0.1-win-x64.exe
 ) else if "!choice!"=="5" (
